9bdcffe44b3f42fc95199b9373001c0cc75d544f
Lys Sanz Moreta <moreta@di.ku.dk>
2024-11-01 14:09:38 +0100

    added more documentation and moved things around

--- run_HPO_ss.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#Highlight: Semisupervised HPO
-CUDA_VISIBLE_DEVICES=0 python Vegvisir_example.py -n 60 -name viral_dataset17 -lt semisupervised -predefined-partitions True -train True -validate True -test False -generate False -immunomodulate False -num-samples 60 -shuffle False -shuffle-labels False -random False -k-folds 5 -filter-kmers False -encoding blosum -plot-all False -save-all False -st Icore -pretrained-model "None" -hpo True;
